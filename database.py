3aca88274b1975103f4cc5321b2f1596132302d2
Holodi <88102380+Holodi@users.noreply.github.com>
2024-02-05 01:29:59 +0000

    Add files via upload

--- database.py
+++ database.py
@@ -1,6 +1,16 @@
-
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///identifier.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 DATABASE_URL = "sqlite:///identifier.sqlite"
 
